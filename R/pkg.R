a581bc3e77d37cfdd457a1b9fd0d181d9df8a058
Martijn Tennekes <mtennekes@gmail.com>
2011-10-21 00:00:00 +0000

    version 0.5

---
+++ R/pkg.R
@@ -0,0 +1,21 @@
+#' A graphical user interface for the tabplot package
+#'
+#' \tabular{ll}{
+#' Package: \tab tabplotGTK\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.5\cr
+#' Date: \tab 2011-10-21\cr
+#' License: \tab GPL-3\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' This package offers a GTK graphical user interface for the creation of tableplots with the tabplot package.
+#'
+#' The function \code{\link{tableGUI}} is used to start the graphical user interface.
+#'
+#' @name tabplotGTK-package
+#' @docType package
+#' @author Martijn Tennekes \email{mtennekes@@gmail.com} and Edwin de Jonge
+#' @keywords visualization, large datasets, GTK, GUI
+#' @example ../examples/pkg.R
+{}
